316c1f9e26e291d3178549fd06ea0b540e65773e
Varun Gupta <vargupt3@in.ibm.com>
2016-11-11 15:06:21 -0600

    fresh workspace

---
+++ Abercrombie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Abercrombie
+//
+//  Created by Varun Gupta on 11/11/16.
+//  Copyright © 2016 Varun GuptaAbercrombie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
